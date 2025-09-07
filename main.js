dd62858862d836c498d2e84f933b212a6bb619ae
Rampravesh <rampraveshkr4545@gmail.com>
2025-09-07 22:08:24 +0530

    git init

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 600,
+    height: 400,
+    title: "Electron js",
+    autoHideMenuBar: true,
+    alwaysOnTop: true,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
