02feefd142296b7f390503b2f0a3462d50bc8faa
Rampravesh <rampraveshkr4545@gmail.com>
2025-09-11 23:17:05 +0530

    01Basic

--- 01Basic/main.js
+++ 01Basic/main.js
@@ -1,21 +1,28 @@
 const { app, BrowserWindow } = require("electron");
+const windowStateKeeper = require("electron-window-state");
 
 function createWindow() {
+  const mainWindowState = windowStateKeeper({
+    defaultWidth: 600,
+    defaultHeight: 400,
+  });
+
   const win = new BrowserWindow({
-    width: 600,
-    height: 400,
-    x: 1200,
-    y: 200,
+    width: mainWindowState.width,
+    height: mainWindowState.height,
+    x: mainWindowState.x,
+    y: mainWindowState.y,
     // frame: false,
     title: "Electron js",
     autoHideMenuBar: true,
-    alwaysOnTop: true,
+    // alwaysOnTop: true,
     webPreferences: {
       nodeIntegration: true,
     },
   });
 
   win.loadFile("index.html");
+  mainWindowState.manage(win);
 }
 
 app.whenReady().then(() => {
